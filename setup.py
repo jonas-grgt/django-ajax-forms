edfe65a97c1df184c90c4f7c5a9188654998c208
Jonas Geiregat <jonas@geiregat.org>
2012-09-10 09:09:33 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ajax-validation',
-    version='0.1.4',
+    version='0.0.2',
     description='Provides support for doing validation using Ajax(currently with jQuery) using your existing Django forms.',
     author='Alex Gaynor',
-    author_email='alex.gaynor@gmail.com',
-    url='http://github.com/alex/django-ajax-validation/tree/master',
+    author_email='jonas@geiregat.org',
+    url='https://github.com/jonasgeiregat/django-ajax-form-mixin',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 1 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
