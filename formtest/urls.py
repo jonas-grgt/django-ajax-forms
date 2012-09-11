ae1c9083ad8612effd54d2fbfc4e518cd498b42b
Jonas Geiregat <jonas@geiregat.org>
2012-09-11 23:42:50 +0200

    Removed example proj

--- formtest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from formtest.views import ContactFormView
-from formtest.views import ContactCreateView
-
-urlpatterns = patterns('',
-    url(r'^contact/$', ContactFormView.as_view(), name="contact_form"),
-    url(r'^contactmodel/$', ContactCreateView.as_view(), name="contact_model_form"),
-)
